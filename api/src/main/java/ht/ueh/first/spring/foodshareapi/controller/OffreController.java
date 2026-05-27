decc16841c7750f1751d74cd9512ab9e5cc97cd2
Mikadua <mikeduallydagobert@gmail.com>
2026-05-26 23:22:34 -0400

    modification du CORS pour donner accès à react.

--- api/src/main/java/ht/ueh/first/spring/foodshareapi/controller/OffreController.java
+++ api/src/main/java/ht/ueh/first/spring/foodshareapi/controller/OffreController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/offres")
+@CrossOrigin(origins = "http://localhost:5173")
 public class OffreController {
 
     private final OffreService offreService;
--- api/src/main/java/ht/ueh/first/spring/foodshareapi/controller/ReservationController.java
+++ api/src/main/java/ht/ueh/first/spring/foodshareapi/controller/ReservationController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservations")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ReservationController {
 
     private final ReservationService reservationService;
